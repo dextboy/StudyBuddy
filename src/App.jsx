df76b818c4964bf756b56869b5b7af162b90c3e4
dextboy <dextboy@outlook.com>
2023-05-24 16:09:34 +0800

    Cloned using StudyPal as skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router-dom";
+import AuthRoute from "./components/AuthRoute";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import PasswordReset from "./pages/PasswordReset";
+import Register from "./pages/Register";
+import UpdatePassword from "./pages/UpdatePassword";
+import About from "./pages/About";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NavBar />
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Routes>
+            <Route element={<AuthRoute />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Route>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/passwordreset" element={<PasswordReset />} />
+            <Route path="/update-password" element={<UpdatePassword />} />
+          </Routes>
+        </div>
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
