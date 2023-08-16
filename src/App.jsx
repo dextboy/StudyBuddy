f4758061e560c5e1250e592379c3ebfbbd3b4337
dextboy <dextboy@outlook.com>
2023-08-16 10:09:39 +0800

    Modified app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Register from "./pages/Register";
 import UpdatePassword from "./pages/UpdatePassword";
 import About from "./pages/About";
 import Notes from "./pages/Notes";
-import Papers from "./pages/Papers";
+import Papers from "./pages/srepap";
 
 const App = () => {
   return (
