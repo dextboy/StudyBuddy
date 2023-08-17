53d356998aa02a9a7ed243e7ed30752073dc0df7
dextboy <dextboy@outlook.com>
2023-08-18 01:53:24 +0800

    Modified name to be consistent

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -22,7 +22,7 @@ const NavBar = () => {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand>StudyBuddy</Navbar.Brand>
+        <Navbar.Brand>StudyBuddie</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
